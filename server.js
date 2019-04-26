e597b3f9cc6ee789de2a66afedf558b39754521b
Tony Kieling <tony.kieling@gmail.com>
2019-04-26 15:48:51 -0700

    doing server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+// const router = express.Router();
 
 const app = express();
 const PORT = 3333;
@@ -9,25 +9,37 @@ const PORT = 3333;
 //   res.status(200).send('router');
 // });
 
-app.use((req, res, next) => {
-  console.log('1- middleware');
-  next();
-});
 
 app.use( (req, res, next) => {
-  console.log('2- aux');
+  console.log('use general- middleware');
   // res.status(200).send(`2- middleware return`);
   next();
 });
 
-app.get('/', (req, res) => {
-  console.log('3- method', req.method, 'url', req.url);
-  res.status(200).send(`method= ${req.method}, url=${req.url}`);
+app.use('/asd', (req, res, next) => {
+  console.log('/asd- middleware');
+  next();
+});
+
+app.get('/asd', (req, res) => {
+  console.log('/asd', req.route);
+  res.status(200).send('asd is OK');
 });
 
 
-// router.use((req, res) => console.trace('useX'));
-// router.param((req, res) => console.trace(id));
-// app.param((req, res) => console.trace(id));
+app.get('/', (req, res, next) => {
+  console.log('GET method', req.method, 'url', req.url);
+  res.status(200).send(`method= ${req.method}, url=${req.url}`);
+});
+
+app.get('/', (err, req, res, next) => {
+  console.log('ERROR', err);
+  res.render('error', { error: err });
+});
+
+app.post('/asd', (req, res) => {
+  console.log('POST method', req.method, 'url', req.url);
+  res.status(200).send(`method= ${req.method}, url=${req.url}`);
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
