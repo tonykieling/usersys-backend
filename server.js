c4f8741502831f8506aa55b6b3c762babc184b23
Tony Kieling <tony.kieling@gmail.com>
2019-04-26 20:39:03 -0700

    doing server, adding parser-body

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 // const router = express.Router();
 
 const app = express();
@@ -9,6 +10,11 @@ const PORT = 3333;
 //   res.status(200).send('router');
 // });
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 app.use( (req, res, next) => {
   console.log('use general- middleware');
@@ -23,19 +29,23 @@ app.use('/asd', (req, res, next) => {
 
 app.get('/asd', (req, res) => {
   console.log('/asd', req.route);
-  res.status(200).send('asd is OK');
+  res.send(req.body);
+  // res.status(200).send('asd is OK');
 });
 
+// app.set('temp', 'temporary variable');
+
+// console.log(app.locals);
 
 app.get('/', (req, res, next) => {
   console.log('GET method', req.method, 'url', req.url);
   res.status(200).send(`method= ${req.method}, url=${req.url}`);
 });
 
-app.get('/', (err, req, res, next) => {
-  console.log('ERROR', err);
-  res.render('error', { error: err });
-});
+// app.get('/', (err, req, res, next) => {
+//   console.log('ERROR', err);
+//   res.render('error', { error: err });
+// });
 
 app.post('/asd', (req, res) => {
   console.log('POST method', req.method, 'url', req.url);
