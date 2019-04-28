c42dc6c4d5c5673e1bdb5e3a652efef575c63d3e
Tony Kieling <tony.kieling@gmail.com>
2019-04-28 14:46:58 -0700

    setting database

--- server.js
+++ server.js
@@ -1,43 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const user = require('./aux/userDB.js');
-const logs = require('./aux/logsDB.js');
-const randomID = require('./aux/randomGen.js');
+// const user = require('./database/userDB.js');
+// const logs = require('./database/logsDB.js');
+// const randomID = require('./database/randomGen.js');
+const {createUser, readAllUsers, readByName} = require('./database/tools/crudUser.js');
 
 const app = express();
 const PORT = 3333;
 
-// router.get('/', (req, res) => {
-//   console.log('router');
-//   res.status(200).send('router');
-// });
+app.get('/', (req, res) => {
+  console.log('root directory');
+  res.status(200).send('this is root directory');
+});
 
 // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 
 app.use( (req, res, next) => {
   console.log('use general- middleware');
-  // console.log("user::: ", user);
-  user['oneMore'] = {second: '2nd'};
   // res.status(200).send(`2- middleware return`);
   next();
 });
 
-app.use('/asd', (req, res, next) => {
-  console.log('/asd- middleware');
-  next();
+// it gets all users from the db
+app.get('/users', (req, res) => {
+  res.send(readAllUsers());
+})
+
+
+// it gets the user by their name
+app.get('/userByName/:name', (req, res) => {
+  const tempName = req.params.name;
+console.log("tempName: ", tempName);
+  res.send(readByName(tempName));
+
+})
+
+
+
+
+app.get('/users.json', (req, res) => {
+  res.json(user)
 });
 
-app.get('/asd', (req, res) => {
-  console.log('user', user);
-  res.send(req.query.name);
-  // res.status(200).send('asd is OK');
-});
-
-// app.set('temp', 'temporary variable');
+// app.get('/user/:name', (req, res) => {
+//   const tempUser = user[req.params.name];
+//   console.log('user', tempUser);
+//   res.send(tempUser);
+//   // res.status(200).send('asd is OK');
+// });
 
 // console.log(app.locals);
 
