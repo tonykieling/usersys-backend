df938f5f57f77109526582dd8a0657bcc6921cdb
Tony Kieling <tony.kieling@gmail.com>
2019-04-28 11:24:22 -0700

    doing auxiliary files

---
+++ aux/logsDB.js
@@ -0,0 +1,12 @@
+let logs = {
+
+}
+
+module.exports = logs;
+
+
+// 2- Users' Log
+// id: number
+// user_id: number
+// dt_time_login: dt
+// dt_time_logout: dt
---
+++ aux/userDB.js
@@ -0,0 +1,15 @@
+const randomID = require('./randomGen.js');
+
+const user = {
+  asd: 10,
+  id: randomID()
+};
+
+module.exports = user;
+
+// 1- User
+// id: number
+// name: number
+// email: string
+// password: string
+// user_admin: boolean
