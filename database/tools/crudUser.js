bdb18f683f1a34a0dbfa214a7c1933bf0b4b3577
Tony Kieling <tony.kieling@gmail.com>
2019-06-14 11:08:34 -0700

    now login is working properly

--- database/tools/crudUser.js
+++ database/tools/crudUser.js
@@ -67,40 +67,34 @@ console.log("inside getUsers");
     response.status(200).json(results.rows);
   });
 }
-  
-// login method
+
+// // login method
 login = (request, response) => {
-console.log("inside login method");
+  console.log("inside login method");
   try {
     const receivedUser = request.body;
-    console.log("receivedUser: ", receivedUser);
-    pool.query('SELECT * FROM users WHERE email = $1 AND password = $2',
+    pool.query('SELECT * FROM users WHERE email = $1 AND password = $2', 
       [receivedUser.email, receivedUser.password], (error, result) => {
-        if (error)
-          throw error;
-          response.send(`Hi ${receivedUser.email}`);
+      if (error) {
+        console.log(`error = ${error}`);
+        throw error;
+      }
+
+      // console.log(`result = ${JSON.stringify(result)}`);
+      if (result.rowCount > 0) {
+        response.status(200).send(`Hi ${receivedUser.email}`);
+        return;
+      } else {
+        response.status(400).send("user/password wrong!");
+        return;
+      }
       })
   } catch (err) {
-    console.log(err.message)
+    console.log(err.message);
     response.send("login was bad, try again, please");
   }
 }
 
-// login = (user) => {
-//   const userId = (checkPassword(user));
-//   if (userId === "NoUser") {
-//     recordLog(null, eventType.login_fail);
-//     return false;
-//   }
-
-//   if (userId.status) {
-//     recordLog(userId.id, eventType.login);
-//     return true;
-//   }
-
-//   recordLog(userId.id, eventType.login_fail);
-//   return false;
-// }
 
 // query user by name.
 // It's NOT case sensitive due to the 'toLowerCase' invoked function
