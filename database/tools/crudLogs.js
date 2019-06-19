7e82b4a261265868f4c3fb5efcd30a27fe2006b1
Tony Kieling <tony.kieling@gmail.com>
2019-06-19 11:27:59 -0700

    logPerUser is working but need to get the user id to query either email or id

--- database/tools/crudLogs.js
+++ database/tools/crudLogs.js
@@ -36,48 +36,17 @@ recordLog = (userId, event) => {
 }
 
 
-
-allLogsAndDate = (request, response) => {
-  // postman example1: http://0.0.0.0:3333/allLogs/1?date_start=2019-01-01&date_end=2019-06-18
-  // postman example: http://0.0.0.0:3333/allLogs/1
-  console.log("parameters", request.params)
-  const userAdmin = request.params.userAdmin;
-  pool.query('SELECT * FROM logs ORDER BY date_time ASC', [], (error, result) => {
-    try {
-      if (error) {
-        console.log("error on getting alllogs method");
-        throw error;
-      }
-      if (result.rowCount > 0) {
-        const message = result.rows;
-        const event = eventType.read_all_logins_success;
-        recordLog(userAdmin, event);
-        response.send(message);
-        return;
-      } else {
-        const event = eventType.read_all_logins_fail;
-        recordLog(userAdmin, event);
-        response.send({message: "`allLogs - NO logs`"});
-        return;
-      }
-    } catch (err) {
-      console.log("allLogs error: ", err.message);
-      const event = eventType.read_all_logins_fail;
-      recordLog(userAdmin, event);
-      response.send({message: "Something bad, try it again."});
-    }
-  });
-}
-
 // it gets all logs
-// TODO: read logs basead in parameters such as, per date/time, for a specific user, etc
 // it receives admin's id, which is gonna record who is performing the query
+// and also it's possible to receive a range date (start and end date)
 // it returns an object that holds all logs info
 allLogs = (request, response) => {
   // postman example1: http://0.0.0.0:3333/allLogs/1?date_start=2019-01-01&date_end=2019-06-18
   // postman example: http://0.0.0.0:3333/allLogs/1
   const userAdmin = request.params.userAdmin;
   const { date_start, date_end } = request.query;
+
+  // auxiliary function to proceede the query upon the data received from the app
   handleQuery = (error, result) => {
     try {
       if (error) {
@@ -116,17 +85,19 @@ allLogs = (request, response) => {
 }
 
 
-//
-//
-//
+// it gets logs related a specific user
+// it receives admin's id, which is gonna record who is performing the query
+// and also it's possible to receive a range date (start and end date)
+// it returns an object that holds all logs info for that specific user
 logPerUser = (request, response) => {
+  // postman example1: http://0.0.0.0:3333/logPerUser/1?user=tao@email.com&date_start=2019-01-01&date_end=2019-06-18
+  // postman example: http://0.0.0.0:3333/logPerUser/1
+
   const userAdmin = request.params.userAdmin;
-  const user = request.query.user;
-  console.log("userAdmin", userAdmin)
-  console.log("user", user)
-  // response.send("OK");
-  // return;
-  pool.query('SELECT * FROM logs WHERE userid = $1 ORDER BY date_time ASC', [user], (error, result) => {
+  const { user, date_start, date_end } = request.query;
+
+  // auxiliary function to proceede the query upon the data received from the app
+  handleQuery = (error, result) => {
     try {
       if (error) {
         console.log("error on getting logPerUser method");
@@ -150,13 +121,22 @@ logPerUser = (request, response) => {
       recordLog(user, event);
       response.send({message: "Something bad, try it again."});
     }
-  });
+  }
+
+  if (date_start)
+    pool.query('SELECT * FROM logs WHERE userid = $1 AND date_time >= $2 AND date_time <= $3 ORDER BY date_time ASC', [user, date_start, date_end], (error, result) => {
+      handleQuery(error, result);
+    });
+  else
+    pool.query('SELECT * FROM logs WHERE userid = $1 ORDER BY date_time ASC', [user], (error, result) => {
+      handleQuery(error, result);
+    });
+
 }
 
 
 module.exports = {
   recordLog,
   allLogs,
-  allLogsAndDate,
   logPerUser
 };
