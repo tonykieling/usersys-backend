c202f089eb605b7f17a442b2f18d6c7bd326b503
Tony Kieling <tony.kieling@gmail.com>
2019-07-19 15:01:04 -0700

    initial production settings

---
+++ public/precache-manifest.8cb3a1d1579fe42c90c72b8fb1a76a4a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec219a38272e47bccb62ae1c81c45ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79a64951b34450f9a2dd",
+    "url": "/static/css/2.56ffb470.chunk.css"
+  },
+  {
+    "revision": "86db3c6118789821aa2c",
+    "url": "/static/css/main.5f8bdec6.chunk.css"
+  },
+  {
+    "revision": "79a64951b34450f9a2dd",
+    "url": "/static/js/2.944d912c.chunk.js"
+  },
+  {
+    "revision": "86db3c6118789821aa2c",
+    "url": "/static/js/main.be0ce090.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3622982fad7ecb5f0c1fe1fd9e1a8faa",
+    "url": "/static/media/IMG-bob_1.3622982f.jpg"
+  },
+  {
+    "revision": "edfdc42c5ae4c6b1057946335514ed15",
+    "url": "/static/media/IMG-jon_3.edfdc42c.jpg"
+  },
+  {
+    "revision": "67f44296cd366136779e87026fddfa6d",
+    "url": "/static/media/IMG-kiko_6.67f44296.jpg"
+  },
+  {
+    "revision": "3622982fad7ecb5f0c1fe1fd9e1a8faa",
+    "url": "/static/media/IMG-nima_21.3622982f.jpg"
+  },
+  {
+    "revision": "3622982fad7ecb5f0c1fe1fd9e1a8faa",
+    "url": "/static/media/IMG-pepe_19.3622982f.jpg"
+  },
+  {
+    "revision": "f75e9a3aa4c508d117412f7974b0c69d",
+    "url": "/static/media/IMG-sue_2.f75e9a3a.jpg"
+  },
+  {
+    "revision": "3622982fad7ecb5f0c1fe1fd9e1a8faa",
+    "url": "/static/media/IMG-x_20.3622982f.jpg"
+  },
+  {
+    "revision": "f75e9a3aa4c508d117412f7974b0c69d",
+    "url": "/static/media/OIMG-sue_2.f75e9a3a.jpg"
+  },
+  {
+    "revision": "bf25c1082d040681082f3a628d318d22",
+    "url": "/static/media/bob.bf25c108.jpg"
+  },
+  {
+    "revision": "3622982fad7ecb5f0c1fe1fd9e1a8faa",
+    "url": "/static/media/canada.3622982f.png"
+  },
+  {
+    "revision": "0de3c088feb9585d8265d3a2d34dd9d0",
+    "url": "/static/media/defaultPicture.0de3c088.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "67f44296cd366136779e87026fddfa6d",
+    "url": "/static/media/kiko.67f44296.jpg"
+  },
+  {
+    "revision": "edfdc42c5ae4c6b1057946335514ed15",
+    "url": "/static/media/someone.edfdc42c.png"
+  },
+  {
+    "revision": "f75e9a3aa4c508d117412f7974b0c69d",
+    "url": "/static/media/sue.f75e9a3a.jpg"
+  }
+]);
